6baf086467a6053e538872a9401ea222ecc8ee31
Evrett <evrettupshall@gmail.com>
2022-10-13 17:14:18 -0400

    Camera Movement

---
+++ New Dawn Aether Hearts/Assets/Scripts/CameraController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.InputSystem;
+
+/*
+ Refrence
+https://www.youtube.com/watch?v=tGAaj824i4s&t=130s
+Used to Learn how to move the camera based on mouse position
+ */
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController instance;
+
+    public float movespeed;
+    public float thickness;
+
+    //PlayerInput InputAction;
+
+    public bool freeze = true;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            freeze = !freeze;
+        }
+
+
+        Vector3 Pos = transform.position;
+
+        if (freeze == false)
+        {
+            //up
+            if (Input.mousePosition.y >= Screen.height - thickness)
+            {
+                Pos.z += movespeed * Time.deltaTime;
+            }
+
+            //down
+            if (Input.mousePosition.y <= Screen.height - thickness)
+            {
+                Pos.z -= movespeed * Time.deltaTime;
+            }
+
+            //right
+            if (Input.mousePosition.x >= Screen.height - thickness)
+            {
+                Pos.x += movespeed * Time.deltaTime;
+            }
+
+            //left
+            if (Input.mousePosition.x <= Screen.height - thickness)
+            {
+                Pos.x -= movespeed * Time.deltaTime;
+            }
+
+            transform.position = Pos;
+        }
+
+    }
+}
