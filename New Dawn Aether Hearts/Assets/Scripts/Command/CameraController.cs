514030e340d3873ee00da9acea1cf2109a9f9d1b
Robbie Strauch <robbie.strauch@gmail.com>
2022-11-12 08:55:24 -0500

    Camera Controller Fix

--- New Dawn Aether Hearts/Assets/Scripts/Command/CameraController.cs
+++ New Dawn Aether Hearts/Assets/Scripts/Command/CameraController.cs
@@ -90,7 +90,7 @@ public class CameraController : MonoBehaviour
             }
 
             //right
-            if (Input.mousePosition.x >= Screen.height - thickness)
+            if (Input.mousePosition.x >= Screen.width - thickness)
             {
                 Pos.x += movespeed * Time.deltaTime;
             }
