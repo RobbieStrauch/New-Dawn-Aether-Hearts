869872e3cf9c7004e2b6fcf9dc601139af27b55e
Robbie Strauch <robbie.strauch@gmail.com>
2022-11-13 08:05:54 -0500

    State Pattern References

--- New Dawn Aether Hearts/Assets/Scripts/State/IState.cs
+++ New Dawn Aether Hearts/Assets/Scripts/State/IState.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Reference: https://www.youtube.com/watch?v=dWrRb9pO8aQ&t=932s
+
 public interface IState
 {
     void Enter();
